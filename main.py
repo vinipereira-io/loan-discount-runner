0ded1c13865f2a436746b915fd3fa5db889ae6a0
Vini Pereira <vini@Vinis-MacBook-Air.local>
2024-02-05 22:41:52 +1100

    capture all parameters to calculate initial loan scenario

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
 from loan_calculator import calculate_expected_repayment_schedule
+from datetime import datetime, timedelta
+from dateutil.parser import parse
 
 print('------------------------------------------------\nWelcome to DiscountRunner!\n------------------------------------------------')
 print('This program helps you simulate the impact of interest rate discounts in your loan.\nFollow the instructions to calculate your own scenarios:')
 
+# loan_amount, repayment_frequency, number_of_repayments, interest_rate, establishment_date, first_repayment_date
+
+loan_amount = input('How much are you borrowing? (in AUD; min 3000; max 50000) ')
+loan_amount = float(loan_amount)
+if loan_amount < 3000 or loan_amount > 50000:
+    raise ValueError('Loan amount is out of acceptable range, it needs to be between 3,000 and 50,000')
+
+repayment_frequency = input('How often do you want to make payments on your loan? (monthly, fortnightly or weekly) ').lower()
+if repayment_frequency not in ['monthly', 'fortnightly', 'weekly']:
+    raise ValueError('Invalid frequency')
+
+term_in_months = input('How long do you need to repay the loan (in months; min 12; max 72) ')
+term_in_months = int(term_in_months)
+if term_in_months < 12 or term_in_months > 72:
+    raise ValueError('Invalid term, it needs to be between 12 and 72 months')
+
+if repayment_frequency == 'monthly':
+    number_of_repayments = term_in_months
+elif repayment_frequency == 'fortnightly':
+    number_of_repayments = round(term_in_months * (26 / 12), 0)
+elif repayment_frequency == 'weekly':
+    number_of_repayments = round(term_in_months * (52 / 12), 0)
+
+interest_rate = input('What interest rate are being charged per year? (min 25%; max 50% )').strip('%')
+interest_rate = float(interest_rate.strip('%')) / 100
+if interest_rate < 0.25 or interest_rate > 0.5:
+    raise ValueError('Interest rate is out of acceptable range, it needs to be between 25 and 50% ')
+
+establishment_date = input('When was this loan funded? (dd/mm/yyyy) ')
+establishment_date = datetime.strptime(establishment_date, '%d/%m/%Y')
+
+first_repayment_date = input('When do you want your first repayment to be? (max 30 days after establishment date; dd/mm/yyyy) ')
+first_repayment_date = datetime.strptime(first_repayment_date, '%d/%m/%Y')
+
+if (first_repayment_date - establishment_date).days > 30:
+    raise ValueError('First repayment date can\'t be after 30 days of funding')
+
+establishment_date = establishment_date.strftime('%d/%m/%Y')
+first_repayment_date = first_repayment_date.strftime('%d/%m/%Y')
+
+repayment = calculate_expected_repayment_schedule(loan_amount, repayment_frequency, number_of_repayments, interest_rate, establishment_date, first_repayment_date)['repayment_amount']
+
+print('Your repayment amount is: $' + str(repayment))
